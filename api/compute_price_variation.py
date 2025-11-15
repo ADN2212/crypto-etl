213f419947673a119ec0ff1bfdb619271afbd0b9
Juan Adonis Nunez Collado <adn221294@gmail.com>
2025-11-15 15:11:35 -0400

    second part of the compute_price_variation endpoint completed

--- api/compute_price_variation.py
+++ api/compute_price_variation.py
@@ -1,13 +1,8 @@
 from datetime import datetime
-from db.read_methods import get_price_from, get_price_until
+from db.read_methods import get_price_from, get_price_until, get_min_date_for_coin
 
-# def parse_date(date_str: str) -> datetime:
-#     y, m, d = date_str.split("-")
-#     return datetime(
-#         year = int(y),
-#         month = int(m),
-#         day = int(d)
-#     )
+#Tal vez en un archivo de constantes ???
+#MIN_DATE = datetime(year=MINYEAR, month=1, day=1)
 
 def compute_price_variation(coin_name: str | None, from_date_str = str | None, until_date_str = str | None):
 
@@ -25,37 +20,47 @@ def compute_price_variation(coin_name: str | None, from_date_str = str | None, u
             "message": "No valid format for date"
         }
 
-    if from_date and not until_date:
+    t1 = None
+    t2 = None
 
+    if from_date and until_date == None:
+        print("Case one")
+        t1 = from_date
         t2 = datetime.now()
-
-        firts_price = get_price_from(
-            coin_name = coin_name,
-            start_date = from_date
+    
+    if from_date == None and until_date:
+        print("case 2")
+        t1 = get_min_date_for_coin(coin_name = coin_name)
+        t2 = datetime(
+                year=until_date.year, 
+                month=until_date.month, 
+                day=until_date.day,
+                hour=23,
+                minute=59,
+                second=59
             )
+
+    if from_date and until_date:
+        print("Case 3")
+        t1 = from_date
+        t2 = until_date
+
+    firts_price = get_price_from(coin_name = coin_name, start_date = t1)
+    last_price = get_price_until(coin_name = coin_name, end_date = t2)
         
-        last_price = get_price_until(
-            coin_name = coin_name,
-            end_date = datetime.now()
-        )
-        
-        delta_time = t2 - from_date
+    delta_time = t2 - t1
 
-        # print(firts_price)
-        # print(last_price)
-        # print(delta_time)
-
-        if firts_price == None or last_price == None:
-            return {
-                "message": "no prices found"
-            }
-
-        variation_per_100 = round(((last_price - firts_price) / last_price) * 100, 3)
-
+    if firts_price == None or last_price == None:
         return {
-            "variation": f'{variation_per_100}%' ,
-            "elapsed_time": {
-                "days": delta_time.days,
-                "hours": delta_time.seconds // 3600,
+            "message": "no prices found"
+        }
+
+    variation_per_100 = round(((last_price - firts_price) / last_price) * 100, 3)
+
+    return {
+        "variation": f'{variation_per_100} %',
+        "elapsed_time": {
+            "days": delta_time.days,
+            "hours": round(delta_time.seconds / 3600, 2),
             }
         }
--- db/read_methods.py
+++ db/read_methods.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, MINYEAR
 from db.engine import crypto_data_table, init_data_base
 from sqlalchemy import select
 
@@ -6,6 +6,29 @@ db = init_data_base()
 
 if db == False:
     raise Exception("An error ocurred while trying to connect to the data base.")
+
+
+#Este metodo retorna la fecha mas antigua 
+def get_min_date_for_coin(coin_name: str) -> datetime:
+    with db.connect() as conn:
+        
+        res = conn.execute(select(crypto_data_table.c.scraped_at).where(
+            crypto_data_table.c.coin_name == coin_name,
+        ).order_by(
+            crypto_data_table.c.scraped_at
+            ).limit(1))
+        
+        res = list(res)
+        
+        #print(res)
+        
+        conn.close()
+        
+        if len(res) == 0:
+            return datetime.now()#It makes sence, isnt it ?
+
+        return res[0][0]
+
 
 #Este metodo retorna el precio de la primera fecha in el intervalo:
 # [star_date, now]
@@ -22,25 +45,17 @@ def get_price_from(coin_name: str, start_date: datetime) -> float | None:#This i
         return res[0][0] if len(res) == 1 else None
 
 #Este metodo retorna el ultimo precio de la moneda que cumpla con la fecha dada:
-#Es decir, si la fecha es 2025-11-15, este metodo reotornara el ultimo precio en el intertvalo:
-# [2025-11-15-0-0-0, 2025-11-15-23-59-59]
+
 def get_price_until(coin_name: str, end_date: datetime) -> float | None:
     with db.connect() as conn:
         res = conn.execute(
             select(
                 crypto_data_table.c.price).where(
                     crypto_data_table.c.coin_name == coin_name,
-                    crypto_data_table.c.scraped_at <= datetime(
-                        year=end_date.year,
-                        month=end_date.month,
-                        day=end_date.day,
-                        hour=23,
-                        minute=59,
-                        second=59
-                    ),
+                    crypto_data_table.c.scraped_at <= end_date
                 )
             )
-
+        
         res = list(res)
         conn.close()
 
